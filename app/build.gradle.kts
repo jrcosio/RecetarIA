ce8053639ab86c5bb3863b5a5e998913f3fd88ab
Jose Ramon Blanco <jose.blanco@lisdatasolutions.com>
2025-02-17 14:32:24 +0100

    init

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.lisdatasolutions.recetaria"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.lisdatasolutions.recetaria"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
